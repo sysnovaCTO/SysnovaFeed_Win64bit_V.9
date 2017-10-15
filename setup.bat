b20744421a6f4b4117c325fa06f9da74e05e6f3c
Sysnova <moin@sysnova.com>
2017-10-15 11:53:10 +0600

    Update setup.bat
    New Setup File

--- setup.bat
+++ setup.bat
@@ -1,23 +1,73 @@
-CD "%USERPROFILE%\Downloads\SysnovaFeed_Win64bit-master"
+@ECHO OFF
+cd %~dp0
 
-xcopy /y mkshortcut.vbs "C:\Windows\System32"
+set "error="
 
 xcopy /y H2\* "C:\Program Files (x86)\H2" /s /i
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to copy H2 Database "
+  goto done
+)
+
 
 mkdir "C:\Program Files\GLPK"
 
 xcopy /y glpk-4.63\* "C:\Program Files\GLPK" /s /i
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to copy GLPK "
+  goto done
+)
+
+mkdir "C:\Program Files\SysnovaFeed"
+xcopy /y setup\* "C:\Program Files\SysnovaFeed" /s /i
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to copy  files inside setup directory"
+  goto done
+)
 
 xcopy /y feedDatabase.h2.db "%USERPROFILE%"
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to copy feedDatabase.db file "
+  goto done
+)
+
+
+xcopy /y mkshortcut.vbs "C:\Windows\System32"
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to copy mkshortcut.vbs file"
+  goto done
+)
+
+
+
+
 
 setx CLASSPATH "C:\Program Files\GLPK\w64\glpk-java.jar"
 
-setx path "%path%;C:\Program Files\GLPK\w64" /M
+setx path "%path%C:\Program Files\GLPK\w64" /M
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to set Path in Environment variable for GLPK."
+  goto done
+)
 
-mkshortcut /target:"%USERPROFILE%\Downloads\SysnovaFeed_Win64bit-master\setup\SysnovaFeed.exe" /shortcut:"%USERPROFILE%\Desktop\SysnovaFeed"
+mkshortcut /target:"C:\Program Files\SysnovaFeed\SysnovaFeed.exe" /shortcut:"%USERPROFILE%\Desktop\SysnovaFeed"
+IF %ERRORLEVEL% NEQ 0 (
+  set "error=error : Failed to Create Shortcut of SysnovaFeed.exe"
+  goto done
+)
 
-echo Installation Finished . Please Run the file SysnovaFeed from Desktop .
+echo Installation Finished Successfully. Shortcut Created in  Desktop .
+goto success
 pause
 
+:done
 
+echo %error%
+goto END
+pause
 
+:success
+pause
+
+:END
+pause
